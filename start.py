23d19601210969a331b42ac406c49fc04c85e557
arainx1 <44363851+arainx1@users.noreply.github.com>
2019-07-08 17:34:35 +0500

    Create start.py

---
+++ start.py
@@ -0,0 +1,46 @@
+import random
+
+import sys
+
+import time
+loginnow = "first.py"
+def mengetik(s):
+
+    for c in s + '\n':
+
+        sys.stdout.write(c)
+
+        sys.stdout.flush()
+
+        time.sleep(random.random() * 0.2)
+
+mengetik('Compiled By Daniyal_Arain.............')
+
+mengetik('.')
+
+mengetik('.')
+
+mengetik('trying to request access...')
+
+mengetik('..WHAT,S HAPPENING................')
+
+mengetik('.WHO Are You ...... Request to Access From Daniyal Arain Server')
+
+mengetik('.Requesting....')
+
+mengetik('......................................')
+
+mengetik('..Request Accepted.!.')
+
+mengetik('...')
+
+mengetik('...')
+
+mengetik('Access Granted')
+
+mengetik('100%')
+
+mengetik ('Arain~Server+ip6_1337ok+80)')
+
+mengetik (' Successfully All Done ~ Relaxd & Enjoy ')
+file = open(loginnow, 'r')
